9c266a2fac72982aed3d839c326761832a6772af
nadun weerakkody <weerakkody.kn@gmail.com>
2024-03-29 17:52:47 +0530

    added ability to edit contacts

---
+++ frontend/src/Pages/ContactsPage.jsx
@@ -0,0 +1,57 @@
+import Contacts from "../Components/Contacts";
+import { useAuthContext } from "../Hooks/useAuthContext";
+
+function ContactsPage({ data }) {
+  const { user } = useAuthContext();
+
+  async function DeleteBtnHandler(_id, name) {
+    const confirmDelete = window.confirm(
+      `Do you want to delete contact "${name}"`
+    );
+
+    if (confirmDelete) {
+      const res = await fetch("http://localhost:4000/" + _id, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      const json = await res.json();
+
+      if (json) {
+        window.location.reload();
+        alert(`${name} has been deleted successfully!`);
+      }
+    }
+  }
+
+  async function HandleEditBtnClick(values, _id) {
+    // Send new content data to backend
+    const response = await fetch(`http://localhost:4000/${_id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+      body: JSON.stringify(values),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      const json = await response.json();
+      window.location.reload();
+      alert(`${values.name} has been saved successfully!`);
+    }
+  }
+
+  return (
+    <Contacts
+      onDeleteBtnClick={DeleteBtnHandler}
+      data={data}
+      onEditBtnClick={HandleEditBtnClick}
+    />
+  );
+}
+
+export default ContactsPage;
